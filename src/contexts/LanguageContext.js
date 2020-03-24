e4b4167f52f449290c6da43bf8df368fa0dda357
Dawood <dawood.ali.16.virgo@gmail.com>
2020-03-25 02:10:30 +0500

    Repalce with the REDUX system

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const context = React.createContext('english');
-console.log(context);
+const Context = React.createContext('english');
+export class LanguageStore extends Component {
+  state = { language: 'english' };
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+  render() {
+    return (
+      <div>
+        <Context.Provider
+          value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+        >
+          {this.props.children}
+        </Context.Provider>
+      </div>
+    );
+  }
+}
 
-export default context;
+export default Context;

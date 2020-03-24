53a28deabe722fe2a39809f354ada28d47c99dac
Dawood <dawood.ali.16.virgo@gmail.com>
2020-03-25 00:24:19 +0500

    context system updated

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends Component {
+  state = {
+    language: 'english'
+  };
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+  render() {
+    return (
+      <div className='ui container'>
+        Select a Language:
+        <i
+          className='flag us'
+          onClick={() => this.onLanguageChange('english')}
+        />
+        <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
+        <ColorContext.Provider value='red'>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
